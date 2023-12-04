ec2ff92835e0cf690260061d0372f1995e6b90a6
vfaithbest <Faith.best@flemingcollege.ca>
2023-12-03 20:35:08 -0500

    Fayb Accordian edits

--- Scripts.js
+++ Scripts.js
@@ -1,1 +1,9 @@
 /*JavaScript Codes */
+/* This code was inspired by the following websites:https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_style_display_toggle  and: https://blog.hubspot.com/website/call-javascript-function-html Code was kinda copied and modified to be simpler  */
+/*Accordian function by FayB */
+function Accordian() {
+    document.getElementsByClassName("ExpandingButton");
+    var PlsWork = this.nextElementSibling;
+    if (PlsWork.style.display === "none"){PlsWork.style.display = "block";
+    }else{PlsWork.style.display = "none";}
+  }

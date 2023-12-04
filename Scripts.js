79ea9b501d9925466581774075c7d196e203101e
MayaIvimey <146374490+MayaIvimey@users.noreply.github.com>
2023-12-03 20:15:21 -0500

    Update Scripts.js

--- Scripts.js
+++ Scripts.js
@@ -1,1 +1,16 @@
 /*JavaScript Codes */
+
+var acc = document.getElementsByClassName("ExpandingButton");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var Article = this.nextElementSibling;
+    if (Article.style.display === "block") {
+      Article.style.display = "none";
+    } else {
+      Article.style.display = "block";
+    }
+  });
+}

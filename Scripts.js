80f626910d25c4b49bad86c0ead78f86c15cfdb9
vfaithbest <Faith.best@flemingcollege.ca>
2023-12-05 15:58:07 -0500

    Fayb

--- Scripts.js
+++ Scripts.js
@@ -1,13 +1,4 @@
 /*JavaScript Codes */
-/* This code was inspired by the following websites:https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_style_display_toggle  and: https://blog.hubspot.com/website/call-javascript-function-html Code was kinda copied and modified to be simpler  */
-/*Accordian function by FayB used for newsletter articles to apear when clicked*/
-function Accordian() {
-    document.getElementsByClassName("ExpandingButton");
-    this.classList.toggle("active");
-    var PlsWork = this.nextElementSibling;
-    if (PlsWork.style.display === "none"){PlsWork.style.display = "block";
-    }else{PlsWork.style.display = "none";}
-  }
 
 var acc = document.getElementsByClassName("ExpandingButton");
 var i;
